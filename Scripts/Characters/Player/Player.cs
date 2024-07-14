ef956dd3ba09f77fbbb8c3a9281ac331c60f0877
Hilton Perantunes <hperantunes@gmail.com>
2024-07-14 17:01:16 -0400

    Play animations

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -12,18 +12,28 @@ public partial class Player : CharacterBody3D
 
     public override void _Ready()
     {
-        
+
     }
 
     public override void _PhysicsProcess(double delta)
     {
         Velocity = new(direction.X, 0, direction.Y);
         Velocity *= speed;
+
         MoveAndSlide();
     }
 
     public override void _Input(InputEvent @event)
     {
         direction = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+
+        if (direction != Vector2.Zero)
+        {
+            animationPlayer.Play("Move");
+        }
+        else
+        {
+            animationPlayer.Play("Idle");
+        }
     }
 }
