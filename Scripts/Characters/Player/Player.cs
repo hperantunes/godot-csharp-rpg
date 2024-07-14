dae3dde418dddda976b954c3cd7e9507e78307cb
Hilton Perantunes <hperantunes@gmail.com>
2024-07-14 12:41:15 -0400

    Move the player

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,13 +3,18 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    private Vector2 direction = new();
+    private int speed = 5;
+
     public override void _PhysicsProcess(double delta)
     {
-        base._PhysicsProcess(delta);
+        Velocity = new(direction.X, 0, direction.Y);
+        Velocity *= speed;
+        MoveAndSlide();
     }
 
     public override void _Input(InputEvent @event)
     {
-        base._Input(@event);
+        direction = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
     }
 }
